81b407b9cbcb9f2bc3d4d0c5e1c16f6767c58f7d
nemanjasijacic <nemanja.sijacic@testpro.io>
2024-03-23 20:53:19 -0400

    Homework-16

---
+++ src/test/java/Homework16.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class Homework16 extends BaseTest {
+    @Test
+    public void testRegistrationNavigation() {
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement registrationLink = driver.findElement(By.cssSelector("(href='registration')"));
+        registrationLink.click();
+
+        String url = "https://qa.koel.app/";
+        driver.get(url);
+        Assert.assertEquals(driver.getCurrentUrl(), url);
+        driver.quit();
+    }
+}
