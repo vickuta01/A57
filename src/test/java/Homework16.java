e6577c350a0b984d08acdd5a38fb41c0233a2b85
Pestrada72 <pearl.estrada@testpro.io>
2024-03-25 03:14:10 -0600

    homework16.java

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -3,26 +3,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
 
-public class LoginTests extends BaseTest {
-    @Test
-    public void loginEmptyEmailPassword() {
+public class Homework16 extends BaseTest {
 
-//      Added ChromeOptions argument below to fix websocket error
+@Test
+public void registrationNavigation() {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://testpro.io/";
+        String url = "https://qa.koel.app/registration";
         driver.get(url);
-
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
-        //test changes
-    }
-}
 
+    }
+    
+}
