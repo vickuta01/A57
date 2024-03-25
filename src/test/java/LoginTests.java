696cea51c197a83c05e69fce1aeed33698b8832e
habtomQA <habtom.fesseha@testpro.io>
2024-03-25 12:31:34 -0700

    L6-M16-framework-demo1
    demo1 excercise

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -2,63 +2,57 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
-
-public class LoginTests extends BaseTest {
+public class LoginTests extends BaseTest{
     @Test
-    public void loginEmptyEmailPassword() {
+    public void loginEmptyEmailPassword(){
+        //added chromeOptions argument bellow to fix websocket error
 
-//      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
+        ChromeOptions options= new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
-        System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
 
-        WebDriver driver = new ChromeDriver(options);
+
+        WebDriver driver=new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        String url = "https://qa.koel.app/";
+        String url="https://qa.koel.app/";
         driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+        Assert.assertEquals(driver.getCurrentUrl(),url);
         driver.quit();
     }
+    @Test
 
-   @Test
     public void loginValidEmailPassword(){
         //pre-condition
         ChromeOptions options=new ChromeOptions();
-        options.addArguments("--allow-remote-origins=*");
-       System.setProperty("webdriver.chrome.driver", "path_to_chromedriver");
-        //chrome driver
-        WebDriver driver = new ChromeDriver(options);
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver=new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         //steps
         //step 1: open browser
-
         String url="https://qa.koel.app/";
         driver.get(url);
         //step 2: enter email
         WebElement emailField= driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
-        emailField.sendKeys(
-         "demo@class.com");
-
+        emailField.sendKeys("demo@class.com");
         //step 3: enter password
         WebElement passwordField= driver.findElement(By.cssSelector("input[type='password']"));
         passwordField.clear();
-        passwordField.sendKeys(
-                "te$t$tudent");
+        passwordField.sendKeys("te$t$tudent");
         //step 4: click on login
-        WebElement loginBtn=driver.findElement(By.cssSelector("button[type='submit]"));
+        WebElement loginBtn=driver.findElement(By.cssSelector("button[type='submit']"));
         loginBtn.click();
         //step 5: expected vs actual
         WebElement avatarIcon=driver.findElement(By.cssSelector("img[class='avatar']"));
         Assert.assertTrue(avatarIcon.isDisplayed());
-        //close the  browser
+        //step 6: close browser
         driver.quit();
-
     }
 }
