da262ccdc31bf6267685fba73cd7f70b44882b18
nemanjasijacic <nemanja.sijacic@testpro.io>
2024-03-27 17:45:59 -0400

    Update Homework16.java

--- src/test/java/Homework16.java
+++ src/test/java/Homework16.java
@@ -10,19 +10,21 @@ import java.time.Duration;
 
 public class Homework16 extends BaseTest {
     @Test
-    public void testRegistrationNavigation() {
+    public void RegistrationNavigation() {
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        String url = "https://qa.koel.app/";
+        driver.get(url);
 
         WebElement registrationLink = driver.findElement(By.cssSelector("(href='registration')"));
         registrationLink.click();
-        String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
+
+        String registrationUrl = "https://qa.koel.app/registration";
+        Assert.assertEquals(driver.getCurrentUrl(), registrationUrl);
         driver.quit();
     }
 }
