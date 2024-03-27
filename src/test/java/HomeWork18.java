14fe29583fc31ddcb45d2e71069ade67e76e5b2d
karina <karina.usmanova01@testpro.io>
2024-03-27 18:00:10 -0400

    Play Song
    Play Song test case

---
+++ src/test/java/HomeWork18.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeWork18 extends BaseTest {
+
+    @Test
+    public void playSong() throws InterruptedException {
+
+
+        navigateToPage();
+        provideEmail("karina.usmanova01@testpro.io");
+        providePassword("YrEdlRVe");
+        clickLoginButton();
+        Thread.sleep(2000);
+
+        clickPlayNextSongButton();
+        Thread.sleep(2000);
+        clickPlayButton();
+        Thread.sleep(2000);
+        showPauseBtn();
+        Thread.sleep(2000);
+        boolean verifyPauseBtn = driver.findElement(By.xpath("//div[@class='side player-controls']//i[@class='fa fa-pause']")).isDisplayed();
+    }
+
+    public void showPauseBtn() {
+        WebElement pauseBtn = driver.findElement(By.xpath("//div[@class='side player-controls']//i[@class='fa fa-pause']"));
+        pauseBtn.isDisplayed();
+    }
+
+    public void clickPlayButton() {
+        WebElement PlayBtn = driver.findElement(By.xpath("//div[@class='side player-controls']//i[@class='fa fa-play']"));
+        PlayBtn.click();
+    }
+
+    public void clickPlayNextSongButton() {
+        WebElement nextSongBtn = driver.findElement(By.xpath("//i[@class='next fa fa-step-forward control']"));
+        nextSongBtn.click();
+    }
+}
