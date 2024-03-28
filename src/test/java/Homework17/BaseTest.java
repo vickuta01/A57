4b4122a5ec5b393bf81feb3c0096ec15c7042c3c
Pestrada72 <pearl.estrada@testpro.io>
2024-03-28 17:05:12 -0600

    Homework17
    Framework17

---
+++ src/test/java/Homework17/BaseTest.java
@@ -0,0 +1,13 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+//import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+}
