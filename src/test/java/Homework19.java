f8b6d04aa2747a4df6ae5b15cb9bbbcf3591da72
Nina1tsa <nina.vantsa@testpro.io>
2024-03-28 19:37:41 -0700

    Homework19

---
+++ src/test/java/Homework19.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Homework19 extends BaseTest{
+    @Test
+    public void deletePlaylist(){
+        String expectedResult = "Deleted playlist \"12.\"";
+
+     provideEmail("nina.vantsa@testpro.io");
+     providePassword("Parasolka2006@");
+     clickSubmitBtn();
+     clickPlayList();
+     clickDeleteBtn();
+     successResult();
+     String actualResult = successResult();
+     Assert.assertEquals(actualResult, expectedResult);
+    }
+
+    private String successResult() {
+        WebElement successResultElement = driver.findElement(By.xpath("//div[@class= 'success show']"));
+        return successResultElement.getText();
+    }
+
+    public void clickDeleteBtn() {
+        WebElement clickDeleteBtn = driver.findElement(By.xpath("//button[@title= 'Delete this playlist']"));
+        clickDeleteBtn.click();
+    }
+
+    public void clickPlayList() {
+        WebElement clickPlayList = driver.findElement(By.xpath("//li[@class= 'playlist playlist']//a[contains(text(), '12')]"));
+        clickPlayList.click();
+    }
+}
