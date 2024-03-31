15de58a9e3134c111b15b8596550a2962070508a
elzatnur <elzat.nurmanbetova@testpro.io>
2024-03-30 20:45:45 -0400

    Homework-17
    Homework-17

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,89 +1,93 @@
+
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
-
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import java.time.Duration;
 
+public class BaseTest{
+    public WebDriver driver;
+    public String url = "https://qa.koel.app/";
 
-    public class BaseTest {
+    @BeforeSuite
+    static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
 
-        public WebDriver driver;
-        public String url = "https://qa.koel.app/";
+    @BeforeMethod
+    public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+        navigateToPage();
+    }
 
-        @BeforeSuite
-        static void setupClass() {
-            WebDriverManager.chromedriver().setup();
-        }
+    public void navigateToPage() {
+        driver.get(url);
+    }
 
-        @BeforeMethod
-        public void launchBrowser(){
-            ChromeOptions options = new ChromeOptions();
-            options.addArguments("--remote-allow-origins=*");
-            driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
-            navigateToPage();
-        }
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
 
-        public void navigateToPage() {
-            //added get() for navigation
-            driver.get(url);
-        }
-        //we are missing a step here to navigate to koel webpage, let's add it
-        @AfterMethod
-        public void closeBrowser(){
-            driver.quit();
-        }
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+    }
 
-        public void clickLoginBtn() {
-            WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-            submit.click();
-        }
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
 
+    void clickLoginBtn() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
 
-        public void provideEmail(String email) {
-            WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-            emailField.clear();
-            emailField.sendKeys(email);
-        }
-        public void providePassword(String password) {
-            WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
-            passwordField.clear();
-           // passwordField.sendKeys();//send keys is empty, no input is being entered
-            passwordField.sendKeys(password); //added password string
-        }
+    public void searchSong(String songName) {
+        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
+        searchField.clear();
+        searchField.sendKeys(songName);
+    }
 
+    public void clickViewAll() {
+        WebElement viewAllBtn = driver.findElement(By.cssSelector("section.songs > h1 > button"));
+        viewAllBtn.click();
 
     }
 
+    public void clickFirstSong() {
+        WebElement firstSong = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//td[@class='title']"));
+        firstSong.click();
+    }
 
+    public void clickAddTo() {
+        WebElement addToBtn = driver.findElement(By.cssSelector("button[class='btn-add-to']"));
+        addToBtn.click();
+    }
 
+    public void addToPlayList(String playListName) {
+        WebElement playListNameField = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//input[@placeholder='Playlist name']"));
+        playListNameField.clear();
+        playListNameField.sendKeys(playListName);
+        WebElement saveBtn = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@type='submit']"));
+        saveBtn.click();
 
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    public String successAlertMessage() {
+        WebElement successAlertMessage = driver.findElement(By.xpath("//div[@class='success show']"));
+        return successAlertMessage.getText();
+    }
+}
--- src/test/java/homeWork17.java
+++ src/test/java/homeWork17.java
@@ -3,79 +3,41 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.util.List;
 
-public class homeWork17 extends BaseTest {
+public class HomeWork17 extends BaseTest {
 
     @Test
-    public void addSongToPlaylist() throws InterruptedException {
-        String expectedSongAddedMessage = "Added 1 song into \"Test Pro Playlist.\"";
-
-
-        //it appears your issue is the nosuchElementException in which selenium webdriver can't locate the input field for email
-        //the issue is our test or automated browser is not navigating to koel webpage
-
-
+    public void addSongToPlaylist() throws Throwable {
+        String successAlertMessage = "1 song is added";
         Thread.sleep(2000);
+        //Login
         provideEmail("elzat.nurmanbetova@testpro.io");
         Thread.sleep(2000);
+
         providePassword("kochkor123-Q!");
+        Thread.sleep(2000);
+
         clickLoginBtn();
         Thread.sleep(2000);
+
         searchSong("Dark");
         Thread.sleep(2000);
-        clickViewAllBtn();
+
+        clickViewAll();
         Thread.sleep(2000);
-        clickAddToBtn();
+
+        clickFirstSong();
         Thread.sleep(2000);
-        Assert.assertEquals(getAddToPlaylistSuccessMsg(), expectedSongAddedMessage);
+
+        clickAddTo();
+        Thread.sleep(2000);
+
+        addToPlayList("Testpro Playlist");
+        Thread.sleep(2000);
+        //Assertion
+        Assert.assertEquals(successAlertMessage(),successAlertMessage() );
     }
-
-    public String getAddToPlaylistSuccessMsg() {
-        WebElement notification = driver.findElement(By.cssSelector("div.success.show"));
-        return notification.getText();
-    }
-
-    public void clickLoginBtn(String songName) {
-        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
-
-        submit.click();
-    }
-
-    public void provideEmail() {
-        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
-        emailField.clear();
-        emailField.click();
-    }
-    public void clickViewAllBtn() {
-        WebElement viewAllBtn = driver.findElement(By.xpath("//button[@data-test='view-all-songs-btn']"));
-        viewAllBtn.click();
-    }
-    //i don'
-    public void searchSong(String s) {
-        //locator for the seat rchField is missing a ' (in the search of type attribute)
-        WebElement searchField = driver.findElement(By.cssSelector("input[type='search']"));
-
-        searchField.clear();
-        //missing input for sendKeys() use the String parameter 's'
-        searchField.sendKeys(s);
-    }
-    public void clickAddToBtn() {
-        WebElement addToBtn = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//button[@data-test='add-to-btn']"));
-        addToBtn.click();
-    }
-
-    //The error now is with clicking the view all button, I would suggest to try or check the locator used
-
-    public void selectFirstSongResult() {
-        WebElement firstSongInResult = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//tr[@class='song-item'][1]"));
-        firstSongInResult.click();
-    }
-
-
 }
-
-
 
 
 
