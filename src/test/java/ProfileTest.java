f465da5946a9681503f3aa7ad2a2d046060c2371
KseniyaVP <164698826+KseniyaVP@users.noreply.github.com>
2024-04-02 13:11:30 -0400

    profile test

--- src/test/java/ProfileTest.java
+++ src/test/java/ProfileTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.UUID;
@@ -17,12 +18,18 @@ public class ProfileTest extends BaseTest {
         clickOnAvatarIcon();
         // generate random name
         String randomName = generateRandomName();
+        System.out.println("Random name is: "+randomName);
         //current password
         provideCurrentPassword("testproA57*");
         //provide new generated name
         provideProfileName(randomName);
         //click on save button
+        clickOnSaveBtn();
+        Thread.sleep(2000);
         //assertions (actual vs expected)
+        WebElement actualProfileName=driver.findElement(By.cssSelector("a.view-profile>span"));
+        Assert.assertEquals(actualProfileName.getText(),randomName);
+
 
 
     }
@@ -31,15 +38,23 @@ public class ProfileTest extends BaseTest {
         WebElement avatarIcon = driver.findElement(By.cssSelector("img[class='avatar']"));
         avatarIcon.click();
     }
-
     public String generateRandomName(){
         return UUID.randomUUID().toString().replace("-","");
     }
 
     public void provideCurrentPassword(String currentPassword){
         WebElement currentPasswordField = driver.findElement(By.cssSelector("input[name='current_password']"));
+        currentPasswordField.clear();
+        currentPasswordField.sendKeys(currentPassword);
     }
     public void provideProfileName(String newProfileName){
         WebElement profileName= driver.findElement(By.cssSelector("input[id='inputProfileName']"));
+        profileName.clear();
+        profileName.sendKeys(newProfileName);
+    }
+    public void clickOnSaveBtn(){
+        WebElement saveBtn =driver.findElement(By.cssSelector("button.btn-submit"));
+        saveBtn.click();
+
     }
 }
