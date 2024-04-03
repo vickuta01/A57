92790b0c9701de7681836b55beb22cc25ea89ba8
elzatnur <elzat.nurmanbetova@testpro.io>
2024-04-03 09:57:30 -0400

    HomeWork-19

---
+++ src/test/java/HomeWork19.java
@@ -0,0 +1,54 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class HomeWork19 extends BaseTest{
+    @Test
+    public void deletePlayList() throws InterruptedException{
+
+        String expectedDeletedMsg = "Deleted playlist \"Testpro playlist.\"";
+
+        //GIVEN
+        provideEmail("elzat.nurmanbetova@testpro.io");
+        Thread.sleep(2000);
+
+        providePassword("kochkor123-Q!");
+        Thread.sleep(2000);
+
+        clickLoginBtn();
+        Thread.sleep(2000);
+        //WHEN
+        clickPlayList("Testpro Playlist");
+        Thread.sleep(1000);
+        deletePlayButton();
+        Thread.sleep(1000);
+        deletePlayButton();
+        Thread.sleep(1000);
+
+        getDeletedMsg();
+        Assert.assertEquals(getDeletedMsg(),expectedDeletedMsg);
+
+    }
+    public void clickPlayList (String playListName){
+        WebElement playListElement = driver.findElement(By.xpath("//a[@href='#!/playlist/93269']"));
+        playListElement.click();
+
+    }
+    public void deletePlayButton (){
+        WebElement deletePlayBtn = driver.findElement(By.xpath("//button[@class='del btn-delete-playlist']"));
+        deletePlayBtn.click();
+    }
+
+    public String getDeletedMsg(){
+        WebElement resultNoticeMsg = driver.findElement(By.xpath("//div[@class='success show']"));
+        return resultNoticeMsg.getText();
+
+    }
+    //
+
+
+
+
+
+}
