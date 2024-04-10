327be923eba665f30a6bc327e61ed5bb27b49397
ENancetestpro <elliott.nance@testpro.io>
2024-04-10 13:56:22 -0400

    Homework 18

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,7 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
@@ -8,13 +10,32 @@ import org.testng.annotations.BeforeSuite;
 
 import java.time.Duration;
 
-
+import static org.bouncycastle.cms.RecipientId.password;
 
 
 public class BaseTest {
-public WebDriver driver = null;
-public String baseURL = "https://qa.koel.app/";
+    public WebDriver driver = null;
+    public String baseURL = "https://qa.koel.app/";
 
+    public void provideEmail(String email) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[placeholder='Email Address']"));
+        emailField.clear();
+        emailField.sendKeys(email);
+//        Thread.sleep(2000);
+    }
+
+        public void providePassword(String password) {
+            WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+            passwordField.clear();
+            passwordField.sendKeys(password);
+//            Thread.sleep(2000);
+        }
+
+            public void clickSubmit() {
+                WebElement submitBtn = driver.findElement(By.cssSelector("[type='submit']"));
+                submitBtn.click();
+//                Thread.sleep(2000);
+            }
 
 @BeforeSuite
 static void setupClass() {
