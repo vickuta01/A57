c8340d89aa08a214fe32355e8c07d6220e103d9c
Agnes A <agnes.albertusiak@testpro.io>
2024-04-11 16:04:37 -0500

    Homework24
    Updates from Live Class and Homework24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -5,7 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -14,11 +19,14 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
 
 import javax.swing.*;
+import java.net.URI;
 import java.time.Duration;
+import java.net.MalformedURLException;
 import java.util.UUID;
 
+
 public class BaseTest {
-    public WebDriver driver;
+    public static WebDriver driver;
     WebDriverWait wait;
     public Actions actions= null;
     public String url = null;
@@ -26,21 +34,24 @@ public class BaseTest {
 
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+
+        //WebDriverManager.chromedriver().setup();
+        //WebDriverManager.firefoxdriver().setup();
     }
 
 
     @BeforeMethod
     @Parameters({"BaseUrl"})
-    public void launchBrowser(String BaseURL){
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--incognito");
-        options.addArguments("--start-maximized");
-        options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+    public void launchBrowser (String BaseURL)throws MalformedURLException{
+        //ChromeOptions options = new ChromeOptions();
+        //options.addArguments("--remote-allow-origins=*");
+        //options.addArguments("--disable-notifications");
+        //options.addArguments("--incognito");
+        //options.addArguments("--start-maximized");
+        //options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
 
-        driver = new ChromeDriver(options);
+        //driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         url = BaseURL;
@@ -51,6 +62,47 @@ public class BaseTest {
     @AfterMethod
     public void closeBrowser(){
         driver.quit();
+    }
+
+    public static WebDriver pickBrowser (String browser) throws MalformedURLException{
+        DesiredCapabilities caps = new DesiredCapabilities();
+        String gridURL = "http://10.0.0.105:4444";
+        switch(browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                edgeOptions.addArguments("--disable-notifications");
+                edgeOptions.addArguments("--incognito");
+                edgeOptions.addArguments("--start-maximized");
+                return driver = new EdgeDriver(edgeOptions);
+
+
+                // GRID cases
+
+            case "grid-firefox":
+                caps.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-chrome":
+                caps.setCapability("browserName", "chrome");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+            case "grid-edge":
+                caps.setCapability("browserName", "MicrosoftEdge");
+                return driver = new RemoteWebDriver(URI.create(gridURL).toURL(), caps);
+
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                chromeOptions.addArguments("--disable-notifications");
+                chromeOptions.addArguments("--incognito");
+                chromeOptions.addArguments("--start-maximized");
+                chromeOptions.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+                return driver = new ChromeDriver(chromeOptions);
+        }
     }
 
     public void clickLoginBtn() {
