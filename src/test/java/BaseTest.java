37194269db2feade3d86deff7d2b64e492ee9f10
elzatnur <elzat.nurmanbetova@testpro.io>
2024-04-15 19:41:28 -0400

    HomeWork24

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,6 +4,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -11,20 +14,25 @@ import org.testng.annotations.*;
 
 import java.time.Duration;
 public class BaseTest{
-    public WebDriver driver;
+    public static WebDriver driver;
     public WebDriverWait wait;
     public String url = "https://qa.koel.app/";
     public Actions actions;
     @BeforeSuite
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+
+        //WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
     }
     @BeforeMethod
     @Parameters({"url"})
     public void launchBrowser(String url) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        driver = new ChromeDriver(options);
+        //ChromeOptions options = new ChromeOptions();
+       // options.addArguments("--remote-allow-origins=*");
+        //driver = new ChromeDriver(options);
+        //driver = new FirefoxDriver();
+        driver = pickBrowser(System.getProperty("browser"));
+
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
@@ -37,6 +45,29 @@ public class BaseTest{
     public void closeBrowser() {
         driver.quit();
     }
+
+    public static WebDriver pickBrowser(String browser){
+        switch(browser){
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+            case "MicrosoftEdge":
+                WebDriverManager.edgedriver().setup();
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new EdgeDriver();
+            default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--remote-allow-origins=*");
+                return driver = new ChromeDriver(chromeOptions);
+
+
+        }
+    }
+
+
+
 
 
     public void searchSong(String songName) {
